8f25e2dde633244ac399ac2a273a29f8da44db01
Ertan Turan <ertanturan@outlook.com>
2019-09-21 14:26:42 +0300

    nuget packages restored and rider controller script created

---
+++ Assets/Scripts/RiderController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RiderController : MonoBehaviour
+{
+
+    public float Speed = 20;
+
+    private Rigidbody2D _rb;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Debug.Log("right arrow");
+            _rb.AddForce(transform.right * Speed * Time.fixedDeltaTime * 100 
+                , ForceMode2D.Force);
+        }
+
+    }
+}
